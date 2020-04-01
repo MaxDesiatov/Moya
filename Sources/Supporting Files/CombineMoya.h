c9fb5e4b33cf3de614ffb03ce90f020d8f1e1320
Max Desiatov <max@desiatov.com>
2020-04-01 16:20:49 +0100

    Add CombineMoya.xcscheme and Xcode target

---
+++ Sources/Supporting Files/CombineMoya.h
@@ -0,0 +1,14 @@
+//
+//  ReactiveMoya.h
+//  ReactiveMoya
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CombineMoya.
+FOUNDATION_EXPORT double CombineMoyaVersionNumber;
+
+//! Project version string for CombineMoya.
+FOUNDATION_EXPORT const unsigned char CombineMoyaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CombineMoya/PublicHeader.h>
